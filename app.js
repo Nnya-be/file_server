79c0aba8da664b7dfe2908239afa8a0134a9d49b
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-05-25 18:30:05 +0000

    modified:path for multer

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter');
-
+const fileRouter = require('./routes/fileRouter')
+const path = require('path');
 app.use(express.json())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/files', fileRouter)
 
 module.exports = app;
