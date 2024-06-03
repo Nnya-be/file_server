54b5f2236a88fc9fff227c5882a4839ab3a73e6f
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-06-03 11:07:58 +0000

    Created: the frontend files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,45 @@
 const User = require('../models/userModel');
+const AppError = require('../utilities/appError');
+const catchAsync = require('../utilities/catchAsync');
 
-exports.getAllUsers = (req, res) =>{
-    res.status(200).json({
-        status: 'success',
-        message:'Gets all the users'
-    })
-}
-exports.getUser = (req, res) =>{
-    res.status(200).json({
-        status: 'success', 
-        message: 'Get one user'
-    })
-}
+module.exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    message: 'Get one user',
+    users,
+  });
+});
+module.exports.getUser = catchAsync(async (req, res, next) => {
+  const { user_id } = req.params;
+
+  if (!user_id) {
+    return next(new AppError('No id found', 400));
+  }
+  const user = await User.findById(user_id);
+  if (!user) {
+    return next(new AppError('No user found!', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    message: 'Get one user',
+    user,
+  });
+});
+
+module.exports.createUser = catchAsync(async (req, res, next) => {
+  if (!req.body) {
+    return next(new AppError('No user details'));
+  }
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    role: req.body.role,
+  });
+  res.status(201).json({
+    status: 'success',
+    user,
+  });
+});
