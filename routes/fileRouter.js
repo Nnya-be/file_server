2ca429cc62e394644f3be7e98fc5eafe092f4f7e
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-06-05 11:16:25 +0000

    Modified: jwt verifications

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -25,5 +25,5 @@ router.get(
   authController.protect,
   fileController.downloadFile
 );
-router.podt('/send/:file_id', authController.protect, fileController.sendFile);
+router.post('/send/:file_id', authController.protect, fileController.sendFile);
 module.exports = router;
